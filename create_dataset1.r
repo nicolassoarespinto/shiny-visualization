82d2f4087149e0e84785b26f592a2b9d0c72d031
nicolassoarespinto <nicolassoarespinto@hotmail.com>
2016-08-26 17:41:41 -0300

    add scripts

---
+++ create_dataset1.r
@@ -0,0 +1,55 @@
+setwd("C:/Users/b2826073/Desktop/EBC/Moeda")
+
+
+library(dplyr)
+library(tidyr)
+library(xlsx)
+library(lazyeval)
+library(ggplot2)
+library(scales)
+library(tidyr)
+
+raw_file<- read.xlsx("raw_data/mc10_moeda_manual_1901_2000.xls", sheetIndex = 1, encoding = "UTF-8", startRow = 3, stringsAsFactors = FALSE)
+
+
+
+names(raw_file) <- raw_file[1,]
+names(raw_file)[1]<- "ano"
+
+user_names <- raw_file[3:dim(raw_file)[1],]
+
+names(raw_file)<- c("ano", "periodo","papel_moeda_emitido","caixa_autoridades","caixa_bancos", "papel_moeda_pp")
+
+raw_file<- mutate(raw_file, moeda = ifelse(is.na(caixa_bancos), ifelse(is.na(periodo), caixa_autoridades, periodo), NA)) 
+
+
+
+
+fill(raw_file, moeda, .direction = "down") %>% 
+  filter(!is.na(periodo)) %>% 
+  filter(!is.na(caixa_autoridades)) %>%
+  fill(ano) -> data_table1
+
+data_table1$periodo <- data_table1$periodo %>% as.numeric %>% as.Date(origin = as.Date("1899-12-30"))
+
+
+
+
+a<- data_table1 %>% filter(!is.na(ano)) %>% gather(key = key, value = value, caixa_bancos, caixa_autoridades, papel_moeda_pp,papel_moeda_emitido)
+
+a$value <- as.numeric(a$value)
+
+a <- a %>% mutate( var=  a$value/lag(a$value) - 1)
+
+write.table(a, "data/data_app1.csv", sep = ";")
+
+#############################
+
+g<- ggplot(a, 
+       aes( x = periodo, y = value, color = moeda)) + 
+  geom_path(group = "key")  + facet_wrap("key")
+
+#############################
+
+g + scale_y_continuous(labels = comma)
+
---
+++ shiny_app1.r
@@ -0,0 +1,66 @@
+ui <-  fluidPage(    
+  
+
+  titlePanel("Desenvolvimento Monetário do Brasil no Século XX"),
+
+  
+  sidebarLayout(      
+    
+
+    sidebarPanel(
+      selectInput("ano_inicial", "De:", 
+                  choices=names(table(a$ano))), 
+      selectInput("ano_final","Até:",
+                  choices= names(table(a$ano))),
+      hr(),
+      helpText("Fonte: IBGE.")
+    ),
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("plot1")  
+    )
+    
+  )
+)
+
+server <- function(input, output) {
+  
+  
+  
+  
+  create_plot <- reactive({
+    
+    
+    b<- a %>% filter(ano>= input$ano_inicial & ano<=input$ano_final) %>% filter(key %in% c("papel_moeda_emitido", "papel_moeda_pp"))
+    
+    g<- ggplot(b, 
+               aes( x = periodo, y = var, color = moeda)) + 
+      geom_path(group = "key", size = 0.5)  + facet_wrap("key")
+    
+    #############################
+    
+    g + scale_y_continuous(labels = comma) + 
+      ggtitle("Desenvolvimento Monetário do Brasil no Século XX - 1900 - 1930") + 
+      theme_bw() +
+      theme(title = element_text(size = 10, face = 'bold', family = "Garamond"),
+            axis.title = element_blank(),
+            axis.text = element_text(family = "Garamond"),
+            panel.grid.minor = element_blank())
+    g<- g + scale_y_continuous(labels = comma)
+    
+    return(g)
+  })
+  
+  output$plot1 <- renderPlot({
+    
+    create_plot()
+    
+  })
+  
+}
+
+app <- shinyApp(ui = ui, server = server)
+
+#Rodar App shiny
+
+runApp(app)
